f79802611e3a34ee4055622f21c2cdcd238cee8d
noaman <barkaninoaman@gmail.com>
2026-09-21 16:53:51 +0200

    Afegeixo comentari a noaman.kt

--- noaman.kt
+++ noaman.kt
@@ -1,4 +1,5 @@
 fun main() {
+    // Missatge de benvinguda
     println("Benvingut, soc Noaman El Bachiri Barkani")
     println("Estic cursant Desenvolupament d'Aplicacions Multiplataforma (DAM)")
 }
